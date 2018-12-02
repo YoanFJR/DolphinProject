ea342de6293a5a2440df4b3a1ede845a98c84afc
GHope <gaetan.bertonniere@epita.fr>
2018-12-02 20:51:54 +0100

    Added functions to modify porfolio related to better navs

--- DolphinProject/DolphinProject/Business/CheckValidity.cs
+++ DolphinProject/DolphinProject/Business/CheckValidity.cs
@@ -48,5 +48,60 @@ namespace DolphinProject.Business
                     stock++;
             return stock >= 10;
         }
+
+        public Value GetFirstWrongAsset(Portfolio p)
+        {
+            Value id = new Value();
+            XMLAccess xml = new XMLAccess();
+            double totalNav = ComputePortfolioNav(p);
+
+            foreach (Actif actif in p.Actifs)
+            {
+                Asset asset = xml.GetAsset(actif.Asset);
+                if (CurrencyConverter.value_exchange(asset.Currency.ToString(), Convert.ToDouble(asset.Nav)) * a.Quantity / totalNav * 100 < 1 || CurrencyConverter.value_exchange(asset.Currency.ToString(), Convert.ToDouble(asset.Nav) * a.Quantity / totalNav * 100) > 10)
+                {
+                    id = asset.Id;
+                    break;
+                }
+            }
+            return id;
+        }
+
+        public Portfolio FixNavAsset(Portfolio p, Value id)
+        {
+            double targeted_nav = ComputePortfolioNav(p) * (5/100);
+            XMLAccess xml = new XMLAccess();
+            Asset asset = xml.GetAsset(Convert.ToInt32(id.value));
+
+            double quantity = targeted_nav / asset.Nav;
+            foreach (Actif actif in p)
+            {
+                if (actif.Asset == Convert.ToInt32(id.value))
+                {
+                    p.Actifs.Remove(actif);
+                    p.Actifs.Add(new Actif()
+                    {
+                        Asset = actif.Asset,
+                        Quantity = Convert.ToInt32(quantity)
+                    });
+                    break;
+                }
+            }
+
+            return p;
+        }
+
+        public void upgradePortfolio (Portfolio p)
+        {
+            while (!CheckPortfolioNav(p))
+            {
+                Value wrong_id = GetFirstWrongAsset(p);
+                p = FixNavAsset(p, wrong_id);
+            }
+            const string BASEURL = "https://dolphin.jump-technology.com:3472/api/v1/";
+            APIAccess api = new APIAccess(BASEURL);
+
+            api.PutPortfolio(p);
+        }
     }
 }
