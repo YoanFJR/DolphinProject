50880f3de121088654ae6cae9a14f728bda19070
GHope <gaetan.bertonniere@epita.fr>
2018-12-02 15:17:19 +0100

    Created class for buiding portfolio

---
+++ DolphinProject/DolphinProject/Business/BuildPortfolio.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DolphinProject.Model;
+using DolphinProject.DataAccess;
+
+namespace DolphinProject.Business
+{
+    class BuildPortfolio
+    {
+        public List<Asset> GetBestSharp(int quantity)
+        {
+            List<Asset> result = new List<Asset>();
+
+            List<Asset> assets = new List<Asset>();
+
+            assets.Sort((a, b) => a.Sharpe < b.Sharpe ? 1 : -1);
+            return result;
+        }
+    }
+}
